a8a5154f009cc24ea57354be68c757c578328abe
YuYang-Dev <79308509+YuYang-Dev@users.noreply.github.com>
2021-11-29 16:47:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9ZL3FQXon/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label; // Sets the name of the object to the result label.
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3); // Set the following label to show the confidence of the model.
+        // toFixed(value) decides the number of places after the decimal.
+    }
+}
---
+++ style.css
@@ -0,0 +1,6 @@
+body
+{
+    background: linear-gradient(rgb(212, 164, 6) , rgb(218, 117, 22));
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+}
